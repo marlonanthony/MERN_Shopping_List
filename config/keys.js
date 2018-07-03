2b9a78e16cc210316783c25051dd169309c72362
borgatajunky <mad1083@gmail.com>
2018-07-02 20:12:43 -0400

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+   mongoURI: 'mongodb://marlon:lisa187@ds159400.mlab.com:59400/mern_shopping_list'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+
+// Item Model
+const Item = require('../../models/Item')
+
+// @routes GET api/items
+// @description Get All Items
+// @access Public
+router.get('/', (req, res) => {
+  Item.find()
+    .sort({ date: -1 })
+    .then(items => res.json(items))
+})
+
+// @routes POST api/items
+// @description Create A Item
+// @access Public
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  })
+
+  newItem.save().then(item => res.json(item))
+})
+
+// @routes DELETE api/items/:id
+// @description Delete A Item
+// @access Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success: false}))
+})
+
+
+module.exports = router
